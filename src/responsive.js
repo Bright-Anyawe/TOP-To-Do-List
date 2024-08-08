be6f67389b9bcd2e7b87e5536c3d8f47d59794dd
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-08-08 23:04:47 +0000

    Add function on sidebar to disppear  opt selected
    The sidebar disppear when the user select an option in the sidebar

--- src/responsive.js
+++ src/responsive.js
@@ -1,42 +1,38 @@
 import { createNewProjectTaskBtn } from "./dom-El-2";
-
-let menuController = document.querySelector(".menuController");
-let closeButton  =  document.querySelector(".close");
-let asideEl  = document.querySelector('main aside');
-let toggleMenuIcon = document.querySelector('#toggleMenuIcon');
+let asideEl = document.querySelector("main aside");
+let toggleMenuIcon = document.querySelector("#toggleMenuIcon");
 let sideBar = document.querySelector("aside#menu");
+let projectDropDown = document.querySelector("#project-dropdown");
+// let projectOption = document.querySelector('#project-dropdown option')
+console.log(projectDropDown);
 // const userListDisplay = document.querySelector("#userlist")
-console.log(asideEl)
+console.log(asideEl);
 
 function toggelMenu() {
-    console.log('toggle element is clicked')
-    toggleMenuIcon.classList.toggle('active');
-    sideBar.classList.toggle("active");
-
+  console.log("toggle element is clicked");
+  toggleMenuIcon.classList.toggle("active");
+  sideBar.classList.toggle("active");
 }
 toggleMenuIcon.addEventListener("click", toggelMenu);
 
-
 function removeSideBar(e) {
-    if (e.target !== toggleMenuIcon && e.target !== sideBar) {
-
+  if (e.target.tagName === "OPTION") {
     toggleMenuIcon.classList.remove("active");
     sideBar.classList.remove("active");
-    }
-        
+  } else if (
+    e.target !== toggleMenuIcon &&
+    e.target !== sideBar &&
+    e.target !== projectDropDown
+  ) {
+    toggleMenuIcon.classList.remove("active");
+    sideBar.classList.remove("active");
+  }
 }
+
+function optionSelected() {
+  toggleMenuIcon.classList.remove("active");
+  sideBar.classList.remove("active");
+}
+
+projectDropDown.addEventListener("change", optionSelected);
 document.addEventListener("click", removeSideBar);
-
-// userListDisplay.append(createNewProjectTaskBtn)
-
-// //Display side on page
-// function displaySideBar() {
-
-
-//             asideEl.display = 'block'
-//         console.log('Display side bar')
-
-    
-
-// }
-// menuController.addEventListener('touchstart', displaySideBar)
